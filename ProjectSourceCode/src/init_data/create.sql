1a4d3732b693958320779354fd0f1f7205392b6a
tschmuckley <trsc9818@colorado.edu>
2024-11-03 17:48:28 -0700

    adding a reviews table

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -2,3 +2,13 @@ CREATE TABLE users (
     username VARCHAR(50) PRIMARY KEY,
     password CHAR(60) NOT NULL
 );
+
+CREATE TABLE reviews (
+    review_id SERIAL PRIMARY KEY,
+    review_text TEXT NOT NULL,
+    reviewer_id INT NOT NULL,
+    reviewee_id INT NOT NULL,
+    rating INT,
+    FOREIGN KEY (reviewer_id) REFERENCES users(user_id),
+    FOREIGN KEY (reviewee_id) REFERENCES users(user_id)
+);
