dde1eeafa98724492117a18765452ab3365df9b5
tschmuckley <trsc9818@colorado.edu>
2024-11-03 20:09:59 -0700

    updating reviews table

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -6,9 +6,9 @@ CREATE TABLE users (
 CREATE TABLE reviews (
     review_id SERIAL PRIMARY KEY,
     review_text TEXT NOT NULL,
-    reviewer_id INT NOT NULL,
-    reviewee_id INT NOT NULL,
-    rating INT,
-    FOREIGN KEY (reviewer_id) REFERENCES users(user_id),
-    FOREIGN KEY (reviewee_id) REFERENCES users(user_id)
+    -- reviewer_id INT NOT NULL,
+    reviewer_name INT NOT NULL,
+    rating INT NOT NULL,
+    -- need to add user_id column for users table
+    -- FOREIGN KEY (reviewer_id) REFERENCES users(user_id),
 );
