d765d7fa9dfe74ca8e87ffee4ae51422803248e5
JacobLehman7842 <lehman.jacob7842@gmail.com>
2024-11-18 15:40:51 -0700

    commiting before group meeting in process of bug fixing create profile

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -3,22 +3,23 @@ CREATE TABLE users (
     userid SERIAL PRIMARY KEY,
     username VARCHAR(50),
     password CHAR(60) NOT NULL
-);
+    );
+
+DROP TABLE IF EXISTS profiles;
 CREATE TABLE profiles(
+
     profile_id SERIAL PRIMARY KEY,
+    userid INT UNIQUE,
     first_name VARCHAR(60),
     last_name VARCHAR(60),
     profile_bio TEXT,
-    profile_picture_url TEXT
+    profile_picture_url TEXT,
+    CONSTRAINT fk_user FOREIGN KEY (userid) REFERENCES users(userid) ON DELETE CASCADE
+    
     -- should add a history of things done or resume?
     -- I will figure out what intails profile tommorow
 );
-CREATE TABLE profile_to_id(
-  profile_id INT NOT NULL,
-  userid INT NOT NULL,
-  FOREIGN KEY (profile_id) REFERENCES profiles (profile_id) ON DELETE CASCADE,
-  FOREIGN KEY (userid) REFERENCES users (userid) ON DELETE CASCADE
-);
+
 
 DROP TABLE IF EXISTS reviews;
 CREATE TABLE reviews (
