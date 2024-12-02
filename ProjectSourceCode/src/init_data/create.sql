f46add880362c871c6c315a10267f52287e4bd8e
JacobLehman7842 <lehman.jacob7842@gmail.com>
2024-12-01 17:44:14 -0700

    new profile table

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -19,22 +19,6 @@ CREATE TABLE profiles(
     -- should add a history of things done or resume?
     -- I will figure out what intails profile tommorow
 );
-CREATE TABLE profiles(
-    profile_id SERIAL PRIMARY KEY,
-    first_name VARCHAR(60),
-    last_name VARCHAR(60),
-    profile_bio TEXT,
-    profile_picture_url TEXT
-    -- should add a history of things done or resume?
-    -- I will figure out what intails profile tommorow
-);
-CREATE TABLE profile_to_id(
-  profile_id INT NOT NULL,
-  userid INT NOT NULL,
-  FOREIGN KEY (profile_id) REFERENCES profiles (profile_id) ON DELETE CASCADE,
-  FOREIGN KEY (userid) REFERENCES users (userid) ON DELETE CASCADE
-);
-
 
 DROP TABLE IF EXISTS reviews;
 CREATE TABLE reviews (
